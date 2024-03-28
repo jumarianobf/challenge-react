1dd9bad523ef6ad25c0c5fd69f224ab3f6c9bc8c
enzo-roch4 <144125363+enzo-roch4@users.noreply.github.com>
2024-03-28 12:27:12 -0300

    add background

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -5,13 +5,16 @@ import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaYoutube } from 'react
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Instagram </p> 
-      <p>Twitter</p>
-      <p>Linkedin</p>
-      <p>Youtube</p>
-      <p>Entre em contato</p>
-
+     <div className='background'>
+          <a href="https://www.instagram.com/salesforcebrasil/" target="_blank"><FaInstagram /></a>
+          <a href="https://twitter.com/salesforce" target="_blank"><FaTwitter /></a>
+          <a href="https://www.linkedin.com/company/salesforce/" target="_blank"><FaLinkedin /></a>
+          <a href="https://www.youtube.com/SalesforceBrasil" target="_blank"><FaYoutube /></a>
+          <p>Entre em contato</p>
+        </div>
     </footer>
+
+    
   );
 }
 
