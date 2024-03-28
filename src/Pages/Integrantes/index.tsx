12cad3fee83557066b94b99dd3e141134f4f5cfe
Julia Mariano <jumarianobf@gmail.com>
2024-03-27 21:02:37 -0300

    coleta de cliques

--- src/Pages/Integrantes/index.tsx
+++ src/Pages/Integrantes/index.tsx
@@ -15,6 +15,11 @@ const Integrantes = () => {
           texto2="RM552713 - 1TDSPA"
           img="src\assets\WhatsApp Image 2023-11-23 at 23.11 1.png"
           linkedin="https://www.linkedin.com/in/julia-mariano-014967288/"/>
+
+          <Card texto1="Leonardo Gaspar Saheb" 
+          texto2="RM553383 - 1TDSPT" 
+          img="src\assets\Foto_Perfil 1.png"
+          linkedin="https://www.linkedin.com/in/leonardo-gaspar-saheb-140669261/"/>
           
           <Card texto1="Leonardo Gaspar Saheb" 
           texto2="RM553383 - 1TDSPT" 
