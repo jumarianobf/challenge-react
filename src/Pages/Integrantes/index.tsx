0d91d8c4515188592b90f893dc6146f0fff04f14
Julia Mariano <jumarianobf@gmail.com>
2024-03-27 21:05:15 -0300

    arrumando card duplicado

--- src/Pages/Integrantes/index.tsx
+++ src/Pages/Integrantes/index.tsx
@@ -20,11 +20,6 @@ const Integrantes = () => {
           texto2="RM553383 - 1TDSPT" 
           img="src\assets\Foto_Perfil 1.png"
           linkedin="https://www.linkedin.com/in/leonardo-gaspar-saheb-140669261/"/>
-          
-          <Card texto1="Leonardo Gaspar Saheb" 
-          texto2="RM553383 - 1TDSPT" 
-          img="src\assets\Foto_Perfil 1.png"
-          linkedin="https://www.linkedin.com/in/leonardo-gaspar-saheb-140669261/"/>
 
           <Card texto1="Enzo Franco Rocha"           
           texto2="RM553643 - 1TDSPT"     
