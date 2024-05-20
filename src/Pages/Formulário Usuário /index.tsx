e6e1b9b9c1185e40d6411402c1c1e25aecaca14d
enzo-roch4 <144125363+enzo-roch4@users.noreply.github.com>
2024-05-20 16:40:17 -0300

    add FormularioUsuario

---
+++ src/Pages/Formulário Usuário /index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState } from 'react';
+import './style.css'
+import Header from "../../Components/Header"
+import Footer from "../../Components/Footer"
+
+function FormularioCadastro() {
+    const [nomeUsuario, setNomeUsuario] = useState('');
+    const [telefoneUsuario, setTelefoneUsuario] = useState('');
+    const [email, setEmail] = useState('');
+    const [regiao, setRegiao] = useState('');
+    const [empresaUsuario, setEmpresaUsuario] = useState('');
+
+}
+
